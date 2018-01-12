d7aef2b627cebb46c8df1d62ac0862bca26d6583
Rob Aird <robkaird@gmail.com>
2018-01-12 10:15:39 -0600

    (refactor) margins on mobile view

--- config/redirect.js
+++ config/redirect.js
@@ -1,7 +1,5 @@
-
-
 module.exports = {
-  // url: 'http://photomapped.herokuapp.com/map/',
   // url: 'http://localhost:4200/map/',
-  url: 'http://localhost:8000/map/',
+  // url: 'http://localhost:8000/map/',
+  url: 'http://photoatlas.co/map/',
 };
